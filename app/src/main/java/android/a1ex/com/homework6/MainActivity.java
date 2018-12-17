3d8878f93d43b81480173748899800bffa421609
A1ex <jaroshov@ukr.net>
2018-12-17 07:47:00 +0200

    17122018

--- app/src/main/java/android/a1ex/com/homework6/MainActivity.java
+++ app/src/main/java/android/a1ex/com/homework6/MainActivity.java
@@ -126,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
                 .setAutoCancel(true)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setDefaults(Notification.DEFAULT_ALL)
-                .setTicker("студент изменен")
+                .setTicker("студент изменен...")
                 .setContentTitle("студент изменен")
                 .setContentText(mStudent.toString())
                 .setWhen(System.currentTimeMillis())
